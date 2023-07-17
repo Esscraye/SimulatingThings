37430cecd9a0edd0c085ad76cc2df3e3e3714b7a
Esscraye <esscrayebat@netcourrier.com>
2023-07-17 13:39:28 +0200

    update directory files

--- Heart/test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = [1, 2, 3, 4, 5] # remplacez les valeurs par les coordonnées x de vos points
-y = [1, 2, 3, 4, 5] # remplacez les valeurs par les coordonnées y de vos points
-
-fig, ax = plt.subplots()
-ax.plot(x, y, 'ro')
-ax.set_title('Points du cœur')
-plt.show()
